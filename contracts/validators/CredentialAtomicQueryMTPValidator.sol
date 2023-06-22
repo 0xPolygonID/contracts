a4bf9af17d427620696784b0069379757e7fc83d
vbasiuk <volodymyr.basiuck@gmail.com>
2023-06-22 23:36:09 +0300

    cleanup

--- contracts/validators/CredentialAtomicQueryMTPValidator.sol
+++ contracts/validators/CredentialAtomicQueryMTPValidator.sol
@@ -7,103 +7,103 @@ import "@iden3/contracts/interfaces/ICircuitValidator.sol";
 import "@iden3/contracts/interfaces/IVerifier.sol";	
 import "@iden3/contracts/interfaces/IState.sol";	
 
-contract CredentialAtomicQueryMTPValidator is OwnableUpgradeable, ICircuitValidator {	
-    string constant CIRCUIT_ID = "credentialAtomicQueryMTPV2OnChain";	
-    uint256 constant CHALLENGE_INDEX = 4;	
+contract CredentialAtomicQueryMTPValidator is OwnableUpgradeable, ICircuitValidator {
+    string constant CIRCUIT_ID = "credentialAtomicQueryMTPV2OnChain";
+    uint256 constant CHALLENGE_INDEX = 4;
 
-    IVerifier public verifier;	
-    IState public state;	
+    IVerifier public verifier;
+    IState public state;
 
-    uint256 public revocationStateExpirationTime;	
+    uint256 public revocationStateExpirationTime;
 
-    function initialize(	
-        address _verifierContractAddr,	
-        address _stateContractAddr	
-    ) public initializer {	
-        revocationStateExpirationTime = 1 hours;	
-        verifier = IVerifier(_verifierContractAddr);	
-        state = IState(_stateContractAddr);	
-        __Ownable_init();	
-    }	
+    function initialize(
+        address _verifierContractAddr,
+        address _stateContractAddr
+    ) public initializer {
+        revocationStateExpirationTime = 1 hours;
+        verifier = IVerifier(_verifierContractAddr);
+        state = IState(_stateContractAddr);
+        __Ownable_init();
+    }
 
-    function setRevocationStateExpirationTime(uint256 expirationTime) public onlyOwner {	
-        revocationStateExpirationTime = expirationTime;	
-    }	
+    function setRevocationStateExpirationTime(uint256 expirationTime) public onlyOwner {
+        revocationStateExpirationTime = expirationTime;
+    }
 
-    function getCircuitId() external pure returns (string memory id) {	
-        return CIRCUIT_ID;	
-    }	
+    function getCircuitId() external pure returns (string memory id) {
+        return CIRCUIT_ID;
+    }
 
-    function getChallengeInputIndex() external pure returns (uint256 index) {	
-        return CHALLENGE_INDEX;	
-    }	
+    function getChallengeInputIndex() external pure returns (uint256 index) {
+        return CHALLENGE_INDEX;
+    }
 
-    function verify(	
-        uint256[] calldata inputs,	
-        uint256[2] calldata a,	
-        uint256[2][2] calldata b,	
-        uint256[2] calldata c,	
-        uint256 queryHash	
-    ) external view returns (bool r) {	
+    function verify(
+        uint256[] calldata inputs,
+        uint256[2] calldata a,
+        uint256[2][2] calldata b,
+        uint256[2] calldata c,
+        uint256 queryHash
+    ) external view returns (bool r) {
         // verify that zkp is valid	
-        require(verifier.verifyProof(a, b, c, inputs), "MTP proof is not valid");	
+        require(verifier.verifyProof(a, b, c, inputs), "MTP proof is not valid");
 
-        require(inputs[2] == queryHash, "query hash does not match the requested one");	
+        require(inputs[2] == queryHash, "query hash does not match the requested one");
 
-        // verify user states	
-        uint256 gistRoot = inputs[5];	
-        uint256 issuerId = inputs[6];	
-        uint256 issuerClaimIdenState = inputs[7];	
-        uint256 issuerClaimNonRevState = inputs[9];	
+        // verify user states
+        uint256 gistRoot = inputs[5];
+        uint256 issuerId = inputs[6];
+        uint256 issuerClaimIdenState = inputs[7];
+        uint256 issuerClaimNonRevState = inputs[9];
 
-        IState.GistRootInfo memory rootInfo = state.getGISTRootInfo(gistRoot);	
+        IState.GistRootInfo memory rootInfo = state.getGISTRootInfo(gistRoot);
 
-        require(rootInfo.root == gistRoot, "Gist root state isn't in state contract");	
+        require(rootInfo.root == gistRoot, "Gist root state isn't in state contract");
 
-        // 2. Issuer state must be registered in state contracts or be genesis	
-        bool isIssuerStateGenesis = GenesisUtils.isGenesisState(issuerId, issuerClaimIdenState);	
+        // 2. Issuer state must be registered in state contracts or be genesis
+        bool isIssuerStateGenesis = GenesisUtils.isGenesisState(issuerId, issuerClaimIdenState);
 
-        if (!isIssuerStateGenesis) {	
-            IState.StateInfo memory issuerStateInfo = state.getStateInfoByIdAndState(	
-               issuerId, issuerClaimIdenState	
-            );	
-            require(issuerId == issuerStateInfo.id, "Issuer state doesn't exist in state contract");	
-        }	
+        if (!isIssuerStateGenesis) {
+            IState.StateInfo memory issuerStateInfo = state.getStateInfoByIdAndState(
+               issuerId, issuerClaimIdenState
+            );
+            require(issuerId == issuerStateInfo.id, "Issuer state doesn't exist in state contract");
+        }
 
-        IState.StateInfo memory issuerClaimNonRevStateInfo = state.getStateInfoById(issuerId);	
+        IState.StateInfo memory issuerClaimNonRevStateInfo = state.getStateInfoById(issuerId);
 
-        if (issuerClaimNonRevStateInfo.state == 0) {	
-            require(	
-                GenesisUtils.isGenesisState(issuerId, issuerClaimNonRevState),	
-                "Non-Revocation state isn't in state contract and not genesis"	
-            );	
-        } else {	
-            // The non-empty state is returned, and it's not equal to the state that the user has provided.	
-            if (issuerClaimNonRevStateInfo.state != issuerClaimNonRevState) {	
-                // Get  the time of the latest state and compare it to the transition time of state provided by the user.	
-                IState.StateInfo memory issuerClaimNonRevLatestStateInfo = state	
+        if (issuerClaimNonRevStateInfo.state == 0) {
+            require(
+                GenesisUtils.isGenesisState(issuerId, issuerClaimNonRevState),
+                "Non-Revocation state isn't in state contract and not genesis"
+            );
+        } else {
+            // The non-empty state is returned, and it's not equal to the state that the user has provided.
+            if (issuerClaimNonRevStateInfo.state != issuerClaimNonRevState) {
+                // Get  the time of the latest state and compare it to the transition time of state provided by the user.
+                IState.StateInfo memory issuerClaimNonRevLatestStateInfo = state
                 .getStateInfoByIdAndState(issuerId,issuerClaimNonRevState);	
 
-                if (	
-                    issuerClaimNonRevLatestStateInfo.id == 0 ||	
-                    issuerClaimNonRevLatestStateInfo.id != issuerId	
-                ) {	
-                    revert("state in transition info contains invalid id");	
-                }	
+                if (
+                    issuerClaimNonRevLatestStateInfo.id == 0 ||
+                    issuerClaimNonRevLatestStateInfo.id != issuerId
+                ) {
+                    revert("state in transition info contains invalid id");
+                }
 
-                if (issuerClaimNonRevLatestStateInfo.replacedAtTimestamp == 0) {	
-                    revert("Non-Latest state doesn't contain replacement information");	
-                }	
+                if (issuerClaimNonRevLatestStateInfo.replacedAtTimestamp == 0) {
+                    revert("Non-Latest state doesn't contain replacement information");
+                }
 
-                if (	
-                    block.timestamp - issuerClaimNonRevLatestStateInfo.replacedAtTimestamp >	
-                    revocationStateExpirationTime	
-                ) {	
-                    revert("Non-Revocation state of Issuer expired");	
-                }	
-            }	
-        }	
+                if (
+                    block.timestamp - issuerClaimNonRevLatestStateInfo.replacedAtTimestamp >
+                    revocationStateExpirationTime
+                ) {
+                    revert("Non-Revocation state of Issuer expired");
+                }
+            }
+        }
 
-        return (true);	
-    }	
-}	
+        return (true);
+    }
+}
