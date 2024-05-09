826fd50407f22f8354d13e640bb7e9a496655349
vbasiuk <volodymyr.basiuck@gmail.com>
2024-05-09 15:54:33 +0300

    add withdraw function

--- contracts/examples/PayExample.sol
+++ contracts/examples/PayExample.sol
@@ -5,11 +5,31 @@ contract PayExample {
         string issuerIdHash;
     }
 
+    address payable public owner;
     mapping (string => Payment) public Payments;
+
+    modifier onlyOwner() {
+        require (msg.sender != owner);
+        _;
+    }
+    
+    constructor() {
+        owner = payable(msg.sender);
+    }
 
     function pay(string calldata sessionIdHash, string calldata issuerIdHash) public payable {
         require(msg.value == 1000000000000000);
         Payment memory p = Payment(issuerIdHash);
         Payments[sessionIdHash] = p;
+    }
+
+    function withdraw(uint amount) public onlyOwner returns(bool) {
+        require(amount < address(this).balance);
+        owner.transfer(amount);
+        return true;
+    }
+
+    function getBalanceContract() public view returns(uint){
+        return address(this).balance;
     }
 }
