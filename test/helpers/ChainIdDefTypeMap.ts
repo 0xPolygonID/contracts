8186a7071c3ac9dece2795b30e13f225033bc17e
vbasiuk <volodymyr.basiuck@gmail.com>
2024-06-13 15:25:22 +0300

    add defoult id type to linea

--- test/helpers/ChainIdDefTypeMap.ts
+++ test/helpers/ChainIdDefTypeMap.ts
@@ -4,4 +4,5 @@ export const chainIdDefaultIdTypeMap = new Map()
   .set(1101, '0x0231') // zkEVM
   .set(1442, '0x0232') // zkEVM testnet
   .set(137, '0x0211') // polygon main
-  .set(59141, "0x0148"); // linea-sepolia
+  .set(59141, "0x0148") // linea-sepolia: iden3 0b0100_0000 | 0b0000_1000
+  .set(59144, "0x0149"); // linea: iden3 0b0100_0000 | 0b0000_1001
