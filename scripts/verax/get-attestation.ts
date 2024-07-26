233c96e9e1821eefbd3898e05b5d6663e4d6470a
vbasiuk <volodymyr.basiuck@gmail.com>
2024-07-26 15:00:32 +0300

    get reputation lvl count script

--- scripts/verax/get-attestation.ts
+++ scripts/verax/get-attestation.ts
@@ -1,23 +1,36 @@
-import { Id } from "@iden3/js-iden3-core";
 import { VeraxSdk } from "@verax-attestation-registry/verax-sdk";
 
-const publicAddress: `0x${string}`= `0x${process.env.SEPOLIA_PUB_ADDRESS}`;
-const privateKey: `0x${string}` = `0x${process.env.SEPOLIA_PRIVATE_KEY}`;
 
 async function main() {
-  const veraxSdk = new VeraxSdk(VeraxSdk.DEFAULT_LINEA_SEPOLIA, publicAddress, privateKey);
-  const attestationId = '0x0000000000000000000000000000000000000000000000000000000000000137';
-  const attestation = await veraxSdk.attestation.getAttestation(attestationId) as {attestationData:  `0x${string}`, subject: `0x${string}`}; 
+  const veraxSdk = new VeraxSdk(VeraxSdk.DEFAULT_LINEA_MAINNET);
+  const attestations: { attestationData: `0x${string}`; subject: `0x${string}` }[] = [];
+  for (let i = 0; i < 40; i++) {
+    const attestationsBatch = (await veraxSdk.attestation.findBy(100, i * 100, {
+      schemaId: '0x021fa993b2ac55b95340608478282821b89398de6fa14073b4d44a3564a8c79d' // PoU schema in LINEA MAIN
+    })) as { attestationData: `0x${string}`; subject: `0x${string}` }[];
+    attestations.push(...attestationsBatch);
+    console.log(attestations.length);
+  }
 
-  console.log(attestation);
-
-  const decoded = 
-    veraxSdk.utils.decode(
+  let repLvl1Count = 0;
+  let repVl2Count = 0;
+  for (let i = 0; i < attestations.length; i++) {
+    const decoded = veraxSdk.utils.decode(
       '(uint64 requestId, uint256 nullifierSessionID, uint256 reputationLevel)',
-      attestation.attestationData as `0x${string}`
-    );
-  console.log(decoded);
-  console.log('sender', attestation.subject);
+      attestations[i].attestationData as `0x${string}`
+    ) as unknown as { requestId: number; nullifierSessionID: string; reputationLevel: bigint }[];
+    const reputationLvlStr = decoded[0].reputationLevel.toString();
+    if (reputationLvlStr == '1') {
+      repLvl1Count++;
+    } else if (reputationLvlStr == '2') {
+      repVl2Count++;
+    } else {
+      throw new Error('Invalid reputation level');
+    }
+  }
+
+  console.log('Reputation level 1 count:', repLvl1Count);
+  console.log('Reputation level 2 count:', repVl2Count);
 }
 
 main()
