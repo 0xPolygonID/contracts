37de73a4606c59fa485f17c1db8eec2f28008ac8
daveroga <daveroga@gmail.com>
2025-01-14 02:51:19 +0100

    fix test with user proofs and cross-chain proofs

--- test/verifiers/erc20-with-universal-verifier.ts
+++ test/verifiers/erc20-with-universal-verifier.ts
@@ -16,6 +16,7 @@ import {
 import { Contract } from 'ethers';
 import { Blockchain, buildDIDType, DidMethod, NetworkId } from '@iden3/js-iden3-core';
 import { StateDeployHelper } from '../helpers/StateDeployHelper';
+import { loadFixture } from '@nomicfoundation/hardhat-toolbox/network-helpers';
 
 const tenYears = 315360000;
 const query = {
@@ -33,11 +34,28 @@ const query = {
   skipClaimRevocationCheck: false
 };
 
+const queryV2 = {
+  schema: BigInt('74977327600848231385663280181476307657'),
+  claimPathKey: BigInt(
+    '20376033832371109177683048456014525905119173674985843915445634726167450989630'
+  ),
+  operator: BigInt(6),
+  slotIndex: BigInt(0),
+  value: ['20020101', ...new Array(63).fill('0')].map((x) => BigInt(x)),
+  circuitIds: [''],
+  queryHash: BigInt(
+    '15045271939084694661437431358729281571840804299863053791890179002991342242959'
+  ),
+  claimPathNotExists: 0,
+  metadata: 'test medatada',
+  skipClaimRevocationCheck: false
+};
+
 describe('ERC 20 test', function () {
   let state: any, sig: any, mtp: any;
   let universalVerifier: Contract, erc20LinkedUniversalVerifier: Contract;
 
-  before(async () => {
+  async function deployContractsFixture() {
     const typ0 = buildDIDType(DidMethod.Iden3, Blockchain.ReadOnly, NetworkId.NoNetwork);
     const typ1 = buildDIDType(DidMethod.Iden3, Blockchain.Polygon, NetworkId.Mumbai);
     const stateDeployHelper = await StateDeployHelper.initialize();
@@ -71,13 +89,16 @@ describe('ERC 20 test', function () {
     await universalVerifier.addValidatorToWhitelist(await sig.getAddress());
     await universalVerifier.addValidatorToWhitelist(await mtp.getAddress());
 
-    await setZKPRequests();
-
     await sig.setProofExpirationTimeout(tenYears);
     await mtp.setProofExpirationTimeout(tenYears);
+  }
+
+  beforeEach(async () => {
+    await loadFixture(deployContractsFixture);
   });
 
   it('Requests count', async () => {
+    await setZKPRequests(query);
     expect(await universalVerifier.getZKPRequestsCount()).to.be.equal(2);
   });
 
@@ -93,11 +114,19 @@ describe('ERC 20 test', function () {
     await erc20VerifierFlowV2('credentialAtomicQuerySigV2OnChain');
   });
 
+  it('Example ERC20 Verifier: set zkp request Sig validator + submit zkp response V2 (fail cross-chain proof)', async () => {
+    await erc20VerifierFlowV2('credentialAtomicQuerySigV2OnChain', true);
+  });
+
   it('Example ERC20 Verifier: set zkp request Mtp validator + submit zkp response V2', async () => {
     await erc20VerifierFlowV2('credentialAtomicQueryMTPV2OnChain');
   });
 
-  async function setZKPRequests() {
+  it('Example ERC20 Verifier: set zkp request Mtp validator + submit zkp response V2 (fail cross-chain proof)', async () => {
+    await erc20VerifierFlowV2('credentialAtomicQueryMTPV2OnChain', true);
+  });
+
+  async function setZKPRequests(requestQuery) {
     async function setRequest(requestId, query, validatorAddress) {
       await universalVerifier.setZKPRequest(requestId, {
         metadata: 'metadata',
@@ -106,7 +135,7 @@ describe('ERC 20 test', function () {
       });
     }
 
-    const query2 = Object.assign({}, query);
+    const query2 = Object.assign({}, requestQuery);
     query2.circuitIds = ['credentialAtomicQuerySigV2OnChain'];
     query2.skipClaimRevocationCheck = false;
     await setRequest(0, query2, await sig.getAddress());
@@ -116,8 +145,8 @@ describe('ERC 20 test', function () {
     await setRequest(1, query2, await mtp.getAddress());
   }
 
-  async function checkValidatorQueryRequest(requestId, validator) {
-    const query2 = Object.assign({}, query);
+  async function checkValidatorQueryRequest(requestId, validator, requestQuery) {
+    const query2 = Object.assign({}, requestQuery);
     query2.circuitIds = [validator];
     query2.skipClaimRevocationCheck =
       validator === 'credentialAtomicQuerySigV2OnChain' ? false : true;
@@ -139,6 +168,8 @@ describe('ERC 20 test', function () {
   async function erc20VerifierFlow(
     validator: 'credentialAtomicQueryMTPV2OnChain' | 'credentialAtomicQuerySigV2OnChain'
   ): Promise<void> {
+    await setZKPRequests(query);
+
     const { inputs, pi_a, pi_b, pi_c } = prepareInputs(
       // eslint-disable-next-line @typescript-eslint/no-var-requires
       validator === 'credentialAtomicQuerySigV2OnChain'
@@ -161,7 +192,7 @@ describe('ERC 20 test', function () {
         ? await erc20LinkedUniversalVerifier.TRANSFER_REQUEST_ID_SIG_VALIDATOR()
         : await erc20LinkedUniversalVerifier.TRANSFER_REQUEST_ID_MTP_VALIDATOR();
 
-    await checkValidatorQueryRequest(requestId, validator);
+    await checkValidatorQueryRequest(requestId, validator, query);
 
     await universalVerifier.submitZKPResponse(requestId, inputs, pi_a, pi_b, pi_c);
     const proofStatus = await universalVerifier.getProofStatus(account, requestId);
@@ -180,8 +211,10 @@ describe('ERC 20 test', function () {
   }
 
   async function erc20VerifierFlowV2(
-    validator: 'credentialAtomicQueryMTPV2OnChain' | 'credentialAtomicQuerySigV2OnChain'
+    validator: 'credentialAtomicQueryMTPV2OnChain' | 'credentialAtomicQuerySigV2OnChain',
+    crossChainProofsFail = false
   ): Promise<void> {
+    await setZKPRequests(queryV2);
     const globalStateMessage = {
       timestamp: BigInt(Math.floor(Date.now() / 1000)),
       idType: '0x01A1',
@@ -191,23 +224,23 @@ describe('ERC 20 test', function () {
 
     const identityStateMessage1 = {
       timestamp: BigInt(Math.floor(Date.now() / 1000)),
-      id: 25530185136167283063987925153802803371825564143650291260157676786685420033n,
-      state: 4595702004868323299100310062178085028712435650290319955390778053863052230284n,
+      id: 23674060690662302859666672288246487392318704165183946026221359507576431105n,
+      state: crossChainProofsFail
+        ? 0
+        : 17963720356087831196970401326056285491593216243607558917899125121151250173336n,
       replacedAtTimestamp: 0n
     };
-
     const identityStateUpdate2 = {
       timestamp: BigInt(Math.floor(Date.now() / 1000)),
-      id: 25530185136167283063987925153802803371825564143650291260157676786685420033n,
-      state: 16775015541053109108201708100382933592407720757224325883910784163897594100403n,
+      id: 23674060690662302859666672288246487392318704165183946026221359507576431105n,
+      state: 4521358091409422119308115615962182429233540714494514399461174721029095638858n,
       replacedAtTimestamp: 1724858009n
     };
-
     const { inputs, pi_a, pi_b, pi_c } = prepareInputs(
       // eslint-disable-next-line @typescript-eslint/no-var-requires
       validator === 'credentialAtomicQuerySigV2OnChain'
-        ? require('./common-data/valid_sig_user_non_genesis_challenge_address.json')
-        : require('./common-data/valid_mtp_user_non_genesis_challenge_address.json')
+        ? require('./common-data/valid_sig_privado_main.json')
+        : require('./common-data/valid_mtp_privado_main.json')
     );
 
     const [signer] = await ethers.getSigners();
@@ -225,7 +258,7 @@ describe('ERC 20 test', function () {
         ? await erc20LinkedUniversalVerifier.TRANSFER_REQUEST_ID_SIG_VALIDATOR()
         : await erc20LinkedUniversalVerifier.TRANSFER_REQUEST_ID_MTP_VALIDATOR();
 
-    await checkValidatorQueryRequest(requestId, validator);
+    await checkValidatorQueryRequest(requestId, validator, queryV2);
 
     const zkProof = packZKProof(inputs, pi_a, pi_b, pi_c);
     const crossChainProofs = packCrossChainProofs(
@@ -236,38 +269,53 @@ describe('ERC 20 test', function () {
     );
     const metadatas = '0x';
 
-    await universalVerifier.submitZKPResponseV2(
-      [
-        {
-          requestId,
-          zkProof: zkProof,
-          data: metadatas
-        }
-      ],
-      crossChainProofs
-    );
+    if (crossChainProofsFail) {
+      await expect(
+        universalVerifier.submitZKPResponseV2(
+          [
+            {
+              requestId,
+              zkProof: zkProof,
+              data: metadatas
+            }
+          ],
+          crossChainProofs
+        )
+      ).to.be.revertedWith('Cross-chain state not found');
+    } else {
+      await universalVerifier.submitZKPResponseV2(
+        [
+          {
+            requestId,
+            zkProof: zkProof,
+            data: metadatas
+          }
+        ],
+        crossChainProofs
+      );
 
-    const proofStatus = await universalVerifier.getProofStatus(account, requestId);
-    expect(proofStatus.isVerified).to.be.true; // check proof is assigned
+      const proofStatus = await universalVerifier.getProofStatus(account, requestId);
+      expect(proofStatus.isVerified).to.be.true; // check proof is assigned
 
-    // check that tokens were minted
-    const balanceBefore = await erc20LinkedUniversalVerifier.balanceOf(account);
-    await erc20LinkedUniversalVerifier.mint(account);
-    const balanceAfter = await erc20LinkedUniversalVerifier.balanceOf(account);
-    expect(balanceAfter - balanceBefore).to.be.equal(BigInt('5000000000000000000'));
+      // check that tokens were minted
+      const balanceBefore = await erc20LinkedUniversalVerifier.balanceOf(account);
+      await erc20LinkedUniversalVerifier.mint(account);
+      const balanceAfter = await erc20LinkedUniversalVerifier.balanceOf(account);
+      expect(balanceAfter - balanceBefore).to.be.equal(BigInt('5000000000000000000'));
 
-    // if proof is provided second time, address is not receiving airdrop tokens, but no revert
-    await universalVerifier.submitZKPResponseV2(
-      [
-        {
-          requestId,
-          zkProof: zkProof,
-          data: metadatas
-        }
-      ],
-      crossChainProofs
-    );
+      // if proof is provided second time, address is not receiving airdrop tokens, but no revert
+      await universalVerifier.submitZKPResponseV2(
+        [
+          {
+            requestId,
+            zkProof: zkProof,
+            data: metadatas
+          }
+        ],
+        crossChainProofs
+      );
 
-    await erc20LinkedUniversalVerifier.transfer(account, 1); // we send tokens to ourselves, but no error because we sent proof
+      await erc20LinkedUniversalVerifier.transfer(account, 1); // we send tokens to ourselves, but no error because we sent proof
+    }
   }
 });
